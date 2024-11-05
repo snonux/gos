3551fd060242b5c0ca07d4cc371b1032d0983e38
Paul Buetow <paul@buetow.org>
2024-11-05 21:59:03 +0200

    fix LinkedIn escapes

--- internal/platforms/linkedin/escapes_test.go
+++ internal/platforms/linkedin/escapes_test.go
@@ -7,7 +7,7 @@ import (
 func TestLinkedInEscapes(t *testing.T) {
 	var (
 		input    = `This is a test message with special characters: " {} @ [] () <> # \ * _ ~ |`
-		expected = `This is a test message with special characters: \" \{\} @ \[\] \(\) \<\> # \\ \* \_ \~ \|`
+		expected = `This is a test message with special characters: " \{\} @ \[\] \(\) \<\> # \\ \* \_ \~ \|`
 	)
 	if escaped := escapeLinkedInText(input); escaped != expected {
 		t.Errorf("expected '%s' but got '%s'", expected, escaped)
