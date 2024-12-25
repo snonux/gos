17b6631a084062f06dcd39da44bedcd6bc26a839
Paul Buetow <paul@buetow.org>
2024-12-25 22:43:31 +0200

    can fetch summary files.

--- internal/summary/summary.go
+++ internal/summary/summary.go
@@ -12,23 +12,39 @@ import (
 
 func Run(ctx context.Context, args config.Args) error {
 	colour.Infoln("Generating summary for", args.SummaryFor)
-	entries := make(map[string]entry.Entry)
+
+	entries, err := deduppedEntries(args)
+	if err != nil {
+		return err
+	}
+	fmt.Println(entries)
+
+	return nil
+}
+
+func deduppedEntries(args config.Args) ([]entry.Entry, error) {
+	dedup := make(map[string]struct{})
+	var entries []entry.Entry
 
 	for _, dateStr := range args.SummaryFor {
 		glob := filepath.Join(args.GosDir, "db/platforms/*/", fmt.Sprintf("*%s*-??????.posted", dateStr))
 		paths, err := filepath.Glob(glob)
 		if err != nil {
-			return err
+			return entries, err
 		}
 
 		for _, path := range paths {
 			en, err := entry.New(path)
 			if err != nil {
-				return err
+				return entries, err
 			}
-			// TODO: Dedup here by name
-			entries[en.Name()] = en
+			if _, ok := dedup[en.Name()]; ok {
+				continue
+			}
+			dedup[en.Name()] = struct{}{}
+			entries = append(entries, en)
 		}
 	}
-	return nil
+
+	return entries, nil
 }
