c0a710cca29fc2c2ed641d8c364ef971f934d61b
Paul Buetow <paul@buetow.org>
2025-01-19 10:04:05 +0200

    found another issue

--- internal/summary/summary_test.go
+++ internal/summary/summary_test.go
@@ -10,8 +10,9 @@ func TestGemtextLink(t *testing.T) {
 		"https://example.org": "=> https://example.org example.org",
 		"https://example.org/some/very/long/link/here?with=a&free=of&parameters=here": "=> https://example.org/some/very/long/link/here?with=a&free=of&parameters=here example.org/s...rameters=here",
 
+		// TODO: Refactor, so that internal links refer to ./ and that the suffix .gmi and/or .html is hidden if possible
 		"beer://foo.zone":                               "=> beer://foo.zone foo.zone",
-		"http://foo.zone":                               "=> gemini://foo.zone foo.zone",
+		"http://foo.zone":                               "=> one foo.zone",
 		"https://foo.zone/index.html":                   "=> gemini://foo.zone/index.gmi foo.zone/index.gmi",
 		"https://foo.zone/gemtext/this-is-awesome.html": "=> gemini://foo.zone/gemtext/this-is-awesome.gmi foo.zone/gemt...s-awesome.gmi",
 	}
