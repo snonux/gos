990dd13e56264429cf75ab539cbb63b3c2d6bbcb
Paul Buetow <paul@buetow.org>
2025-05-02 10:17:34 +0300

    change default

--- internal/main.go
+++ internal/main.go
@@ -27,7 +27,7 @@ func Main(composeModeDefault bool) {
 	minQueued := flag.Int("minQueued", 4, "Minimum of queued items until printing a warn message!")
 	maxDaysQueued := flag.Int("maxDaysQueued", 1000, "Maximum days worth of queued posts until target++ and pauseDays--")
 	pauseDays := flag.Int("pauseDays", 3, "How many days until next post can be posted?")
-	runInterval := flag.Int("runInterval", 12, "How many hours to wait for the next run.")
+	runInterval := flag.Int("runInterval", 6, "How many hours to wait for the next run.")
 	lookback := flag.Int("lookback", 30, "How many days look back in time for posting history")
 	geminiSummaryFor := flag.String("geminiSummaryFor", "", "Generate a summary in Gemini Gemtext format, format is coma separated string of months, e.g. 202410,202411")
 	geminiCapsules := flag.String("geminiCapsules", "foo.zone", "Comma sepaeated list Gemini capsules. Used by geminiEnable to detect Gemtext links")
